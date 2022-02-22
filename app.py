494e1f2a1dae866b53b139686f545ef889e7293d
Gonza-Gil <gonzalogil123@gmail.com>
2022-02-22 15:52:48 -0300

    Buscar y ver contacto implementados

--- app.py
+++ app.py
@@ -18,28 +18,40 @@ def crear_contacto():
     apellido = input("Ingrese el apellido del contacto: ")
     numero = input("Ingrese el numero de telefono: ")
     categoria = input("Ingrese la categoria del contacto: ")
-    n = CARPETA + nombre + ' ' + apellido + EXTENSION
+    n = CARPETA + nombre.lower() + apellido.lower() + EXTENSION
     with open(n, 'w', encoding="utf-8") as archivo:
         archivo.write('Nombre: ' + nombre + ' ' + apellido + '\n')
         archivo.write('Número: ' + numero + '\n')
         archivo.write('Categoria: ' + categoria + '\n')
 
+
 def editar_contacto():
     pass
 
-def ver_contacto():
-    pass
 
-def buscar_contacto():
-    pass
+def ver_contacto(file_name):
+    with open(file_name, 'r', encoding='utf-8') as archivo:
+        for line in archivo:
+            print(line, end='')
+
+
+def buscar_contacto(name):
+    aux_list = os.listdir(CARPETA)
+    aux_name = name.lower().replace(' ','')
+    aux_name += EXTENSION
+    if aux_name in aux_list:
+        file_name = aux_list[aux_list.index(aux_name)]
+        file_name = CARPETA + file_name
+        ver_contacto(file_name)
+
 
 def eliminar_contacto():
     pass
         
 
-
 def app():
     crear_directorio()
-    crear_contacto()
+    buscar_contacto('Gonzalo Gil')
     
+
 app()
